1a573cca069562f0271dc085caae18300ff7ad70
nandhan <Nandhan.BV@atkinsglobal.com>
2024-06-29 00:59:07 +0530

    Added all the functionalities

---
+++ src/ui/Home.jsx
@@ -0,0 +1,27 @@
+import CreateUser from '../features/user/CreateUser'
+import Button from './Button'
+import { useSelector } from 'react-redux'
+
+function Home() {
+    const username = useSelector((state) => state.user.username)
+    return (
+        <div className="my-10 px-4 text-center">
+            <h1 className="mb-8 text-center text-xl font-semibold md:text-3xl">
+                The best pizza.
+                <br />
+                <span className="text-yellow-500">
+                    Straight out of the oven, straight to you.
+                </span>
+            </h1>
+            {username === '' ? (
+                <CreateUser />
+            ) : (
+                <Button to="/menu" type="primary">
+                    Continue Ordering , {username}
+                </Button>
+            )}
+        </div>
+    )
+}
+
+export default Home
